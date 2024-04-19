daea60d2954cf266893eda3a9127be21ed4f29b4
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-04-19 21:13:43 +0500

    Update and rename 001_array.js to 001_Задача_на_знание_объектов,_методов_массива.js

--- 001_Задача_на_знание_объектов,_методов_массива.js
+++ 001_Задача_на_знание_объектов,_методов_массива.js
@@ -1,6 +1,6 @@
 Задача на знание объектов, методов массива.
-Дан массив объектов сотрудников, каждый из которых имеет поля **name** (имя), **age** (возраст) и **profession** (профессия). Также дан массив профессий, где каждая профессия представлена объектом с полями **title** (название профессии) и **skills** (массив навыков). Ваша задача:
-
+Дан массив объектов сотрудников, каждый из которых имеет поля **name** (имя), **age** (возраст) и **profession** (профессия). Также дан массив профессий, где каждая профессия представлена объектом с полями **title** (название профессии) и **skills** (массив навыков). 
+Ваша задача:
 1. Найти среди сотрудников человека с профессией "Разработчик".
 2. Найти среди сотрудников всех, кто старше найденного разработчика по полю **age**.
 3. Определить профессию самого старшего среди найденных сотрудников.
@@ -18,7 +18,7 @@ const professions = [
   { title: "Менеджер", skills: ["Управление", "Agile", "Scrum"] }
 ];
 
-1. Через цикл
+1. Решение через цикл
 function findSkillsOfOldestDeveloper(employees, professions) {
   let developer = null;
   for (let i = 0; i < employees.length; i++) {
@@ -29,14 +29,14 @@ function findSkillsOfOldestDeveloper(employees, professions) {
 }
 console.log(findSkillsOfOldestDeveloper(employees, professions));
 
-1. Через метод
+1. Решение через метод
 function findSkillsOfOldestDeveloper(employees, professions) {
   const developer = employees.find((emploee) => emploee.profession === 'Разработчик');
   return developer;
 }
 console.log(findSkillsOfOldestDeveloper(employees, professions));
 
-2. Через цикл
+2. Решение через цикл
 function findSkillsOfOldestDeveloper(employees, professions) {
   const olderDeveloper = [];
   for (let i = 0; i < employees.length; i++) {
@@ -47,7 +47,14 @@ function findSkillsOfOldestDeveloper(employees, professions) {
 }
 console.log(findSkillsOfOldestDeveloper(employees, professions));
 
-3. Через цикл
+2. Решение через метод
+function findSkillsOfOldestDeveloper(employees, professions) {
+  const older = employees.filter(i => i.age > employees[0].age);
+  return older; 
+}
+console.log(findSkillsOfOldestDeveloper(employees, professions));
+
+3. Решение через цикл
 function findSkillsOfOldestDeveloper(employees, professions) {
   let ageMaxProfession = null;
   for (let i = 0; i < employees.length; i++) {
@@ -58,7 +65,14 @@ function findSkillsOfOldestDeveloper(employees, professions) {
 } 
 console.log(findSkillsOfOldestDeveloper(employees, professions));
 
-4. Через цикл
+3. Решение через метод
+function findSkillsOfOldestDeveloper(employees, professions) {
+  const oldest = employees.find(i => i.age > employees[1].age);
+  return oldest.profession;
+}
+console.log(findSkillsOfOldestDeveloper(employees, professions));
+
+4. Решение через цикл
 function findSkillsOfOldestDeveloper(employees, professions) {
   for (let i = 0; i < employees.length; i++) {
     if (professions[i].title === 'Менеджер') {
@@ -68,9 +82,9 @@ function findSkillsOfOldestDeveloper(employees, professions) {
 } 
 console.log(findSkillsOfOldestDeveloper(employees, professions));
 
-4. Через метод
+4. Решение через метод
 function findSkillsOfOldestDeveloper(employees, professions) {
-  const findSkills = professions.find((profession) => profession.title === 'Менеджер');
-  return findSkills;
-} 
+  const object = professions.find(i => i.title === 'Менеджер');
+  return object.skills;
+}
 console.log(findSkillsOfOldestDeveloper(employees, professions));
